a8df40935082e04713fe3e4f87ce80b94593477c
marzelin <marzelin@gmail.com>
2015-11-23 11:16:43 +0100

    ready to implement user authentication

---
+++ image_share.js
@@ -0,0 +1,33 @@
+Images = new Mongo.Collection('images');
+
+if (Meteor.isClient) {  
+
+  Template.images.helpers({images: Images.find({},{'sort': {'dateAdded': -1, 'rating': -1}})});
+  
+  var clicked = false;
+  Template.images.events({
+    'click .js-image': function (event) {
+      // just doing nothing
+    }, 
+    'click .js-del-image': function (event) {
+      $('#' + this._id).hide('slow', function () {        
+        Images.remove({'_id': this.id});
+      });
+    },
+    'click .jsRateImage': function (event) {
+      var rating = $(event.currentTarget).data('userrating');
+      Images.update({'_id': this._id}, {'$set': {'rating': rating}});
+    }
+  });
+  
+  Template.addImgTempl.events({
+    'submit #add-img-form': function (event) {
+      Images.insert({
+        'imgSrc': event.target.imgSrc.value,
+        'imgAlt': event.target.imgAlt.value,
+        'dateAdded' : new Date()
+      });
+      return false;
+    }
+  });
+}
